3900f59b53ce8e6b6e14740e83ee78f2545f3b9f
Sajed <azizisajed@hotmail.co.uk>
2026-03-01 04:13:02 +0000

    Initialise Spring Boot project and Docker Compose

---
+++ adaptivefit-api/src/main/java/com/adaptivefit/AdaptiveFitApplication.java
@@ -0,0 +1,12 @@
+package com.adaptivefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptiveFitApplication.class, args);
+    }
+}
